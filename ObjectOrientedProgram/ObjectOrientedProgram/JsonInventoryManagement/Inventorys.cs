c7524c6d5fb7f0129b877a69fc2409a55f9e9f40
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-16 18:50:10 +0530

    JSON Inventory Management of Rice, Pulses and Wheats

---
+++ ObjectOrientedProgram/ObjectOrientedProgram/JsonInventoryManagement/Inventorys.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ObjectOrientedProgram.JsonInventoryManagement
+{
+    class Inventorys
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public double Price { get; set; }
+
+        //Parameterized Constructor
+        public Inventorys(string Name, double Weight, double Price)
+        {
+            this.Name = Name;
+            this.Weight = Weight;
+            this.Price = Price;
+        }
+    }
+
+    class InventoryDatas
+    {
+        public void Data()
+        {
+            //Display Message
+            Console.WriteLine("Welcome to JSON Inventory Data Management");
+            Console.WriteLine("=========================================");
+
+            //Display Messsages
+            Console.WriteLine("Choose An Option to Perform an Action :");
+            Console.WriteLine("Press 1 : Add Details In Inventory ");
+            Console.WriteLine("Press 2 : Display Inventory Details ");
+            Console.WriteLine("----------------------------------------");
+            int userOption = int.Parse(Console.ReadLine());
+
+            switch (userOption)
+            {
+                case 1: //Add Details
+                        //Create List
+                    List<Inventorys> inventoryList = new List<Inventorys>();
+
+                    Inventorys inventory = new Inventorys("Rice", 500, 60);
+                    Inventorys inventory1 = new Inventorys("Pulses", 300, 80);
+                    Inventorys inventory2 = new Inventorys("Wheats", 1000, 100);
+
+                    //Adding to List
+                    inventoryList.Add(inventory);
+                    inventoryList.Add(inventory1);
+                    inventoryList.Add(inventory2);
+
+                    //Serialize JSON 
+                    string json = JsonConvert.SerializeObject(inventoryList);
+                    File.WriteAllText(@"C:\Users\HP\Desktop\DotNet projects\ObjectOrientedProgram\ObjectOrientedProgram\ObjectOrientedProgram\JsonInventoryManagement\InventoryDetails.json", json);
+
+                    Console.WriteLine("Inventory Details Added SuccessFully in File.");
+                    break;
+
+                case 2:// Display Inventory Items
+
+                    //Display Messages
+                    Console.WriteLine("Dispaly Inventory Items");
+                    Console.WriteLine("-----------------------");
+
+                    //Deserialize Data of JSON file
+                    string datafile = File.ReadAllText(@"C:\Users\HP\Desktop\DotNet projects\ObjectOrientedProgram\ObjectOrientedProgram\ObjectOrientedProgram\JsonInventoryManagement\InventoryDetails.json");
+
+                    List<Inventorys> returnDataObj = JsonConvert.DeserializeObject<List<Inventorys>>(datafile);
+
+                    //Display JSON file Data
+                    foreach (var form in returnDataObj)
+                    {
+
+                        Console.WriteLine("Name : " + form.Name);
+                        Console.WriteLine("Weight : " + form.Weight);
+                        Console.WriteLine("Price : " + form.Price);
+
+                        //Calulation
+                        Console.WriteLine("Total Value of {0} =  {1}: ", form.Name, (form.Weight * form.Price));
+                        Console.WriteLine("---------------------------------");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Input !!!");
+                    break;
+
+            }
+        }
+    }
+}
--- ObjectOrientedProgram/ObjectOrientedProgram/Program.cs
+++ ObjectOrientedProgram/ObjectOrientedProgram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ObjectOrientedProgram.JsonInventory;
+using ObjectOrientedProgram.JsonInventoryManagement;
 using System;
 
 namespace ObjectOrientedProgram
@@ -7,9 +8,41 @@ namespace ObjectOrientedProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcom to Object Oriented Programs");
-            InventoryData obj = new InventoryData();
-            obj.Data();
+            Console.WriteLine("Welcom to Object Oriented Programs\n");
+            
+            while (true)
+            {
+                Console.WriteLine("\nChoose An Option to Perform an Action :");
+                Console.WriteLine("Press 1 : Json Inventory \nPress 2 : Json Inventory Management\nPress 3 : Exit");
+                Console.WriteLine("----------------------------------------");
+                int userOption = int.Parse(Console.ReadLine());
+                switch (userOption)
+                {
+                    case 1:
+                        Console.WriteLine("Display Json Inventory\n");
+                        InventoryData obj = new InventoryData();
+                        obj.Data();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Display Json Inventory Management\n");
+                        InventoryDatas a = new InventoryDatas();
+                        a.Data();
+                        break;
+
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("You Enter an Invalid Number:\n");
+                        break;
+                       
+
+                }
+    
+            }
         }
+            
     }
 }
